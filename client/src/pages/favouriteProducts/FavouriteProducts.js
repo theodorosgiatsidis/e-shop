d900e7b16ee452bf0759a8db1ffccbb785197c0a
Thodoros Giatsidis <46629765+theodorosgiatsidis@users.noreply.github.com>
2022-03-08 13:46:31 +0200

    Favourite-products css

--- client/src/pages/favouriteProducts/FavouriteProducts.js
+++ client/src/pages/favouriteProducts/FavouriteProducts.js
@@ -1,24 +1,27 @@
 import React, { useContext } from "react";
 import { StoreContext } from "../../context/store";
 import Product from "../../components/product/Product";
+import "./favouriteProducts.css";
 
 function FavouriteProducts() {
   const { favouriteProducts } = useContext(StoreContext);
 
   return (
     <div className="fav-products">
-      <h1>Favourite Products</h1>
-      {favouriteProducts.map((product, index) => (
-        <div key={index}>
-          <Product
-            title={product.title}
-            description={product.description}
-            picture={product.picture}
-            id={product._id}
-            price={product.price}
-          />
-        </div>
-      ))}
+      <h1>My Favourite Products</h1>
+      <div className="products">
+        {favouriteProducts.map((product, index) => (
+          <div className="map-products" key={index}>
+            <Product
+              title={product.title}
+              description={product.description}
+              picture={product.picture}
+              id={product._id}
+              price={product.price}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
